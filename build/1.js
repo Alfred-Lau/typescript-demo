a897f9b9e702503fc829cfb171e0ce9e41f3956b
97f_100004392950_liujian <liujian@cai-inc.com>
2019-01-31 19:35:01 +0800

    commit

--- build/1.js
+++ build/1.js
@@ -12,6 +12,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+Object.defineProperty(exports, "__esModule", { value: true });
 var _a;
 var options = {};
 options.name = 'hello';
@@ -92,12 +93,12 @@ function createSquare(config) {
     return newSquare;
 }
 var mySquare = createSquare({
-    color: 'black'
+    color: 'black',
 });
 console.log(mySquare);
 var p1 = {
     x: 10,
-    y: 20
+    y: 20,
 };
 var arr = [1, 2, 3, 4];
 var ro = arr; //定义一个去除所有可变方法的数组，保证数组创建之后再也不能被修改
@@ -243,3 +244,181 @@ var Animal3 = /** @class */ (function () {
     return Animal3;
 }());
 // new Animal3('Cat').age //无法访问
+// 构造函数也可以被标记为protected。这意味着这个类不能在包含它的类外被实例化，但是能被继承
+var Person = /** @class */ (function () {
+    function Person(theName) {
+        this.name = theName;
+    }
+    return Person;
+}());
+// Employee 能够继承Person
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, department) {
+        var _this = _super.call(this, name) || this;
+        _this.department = department;
+        return _this;
+    }
+    Employee.prototype.getElevatorPitch = function () {
+        return "\n      Hello, my name is " + this.name + " and i work in " + this.department + ".\n    ";
+    };
+    return Employee;
+}(Person));
+var howard = new Employee('Howard', 'Sales');
+// let john = new Person('JOHN')   //Constructor of class 'Person' is protected and only accessible within the class declaration.
+// readonly只读属性：必须在声明时或构造函数里被初始化
+var Octopus = /** @class */ (function () {
+    function Octopus(theName) {
+        this.numberOfLegs = 8;
+        this.name = theName;
+    }
+    return Octopus;
+}());
+var dad = new Octopus('Man with the 8 strong legs');
+// dad.name = 's'  只读报错
+// 参数的属性：参数属性通过给构造函数参数添加一个访问限定符来声明。使用private限定一个参数属性回声明并初始化一个私有成员；对于  public和 protected来说也是一样
+var Animal4 = /** @class */ (function () {
+    // 声明和赋值结合
+    function Animal4(name) {
+        this.name = name;
+    }
+    Animal4.prototype.move = function (distance) {
+        console.info(this.name + " moved " + distance);
+    };
+    return Animal4;
+}());
+var a4 = new Animal4('doggy');
+a4.move(100);
+// 存取器：getter setter
+// 没有getter， setter的版本
+var Employee2 = /** @class */ (function () {
+    function Employee2() {
+    }
+    return Employee2;
+}());
+var employee2 = new Employee2();
+employee2.fullName = 'Bob smith';
+if (employee2.fullName) {
+    console.info(employee2.fullName);
+}
+// 设置了 getter， setter
+var password = 'secret password';
+var Employee3 = /** @class */ (function () {
+    function Employee3() {
+    }
+    Object.defineProperty(Employee3.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (password && password === 'secret password') {
+                this._fullName = newName;
+            }
+            else {
+                console.error('not authorized');
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee3;
+}());
+var employee3 = new Employee3();
+employee3.fullName = "Ted";
+if (employee3.fullName) {
+    console.info(employee3.fullName);
+}
+// 对于存取器，我们有下面几点需要注意：首先，存取器需要我们把编译器设置为输出 ecmascript 5或者更高。其次，只带有 get不带有 set的存储器自动被推断为 readonly。这在从代码生成.d.ts文件是有帮助的，因为利用这个属性的用户会看到不允许够改变它的值
+// 静态属性：类分为实例成员(仅当类被实例化的时候才会被初始化的属性)，静态属性(存在于类本身上面而不是类的实例上)
+var Grid = /** @class */ (function () {
+    function Grid(scale) {
+        this.scale = scale;
+    }
+    // 注意此处参数的声明方式
+    Grid.prototype.calculateDistanceFromOrigin = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    };
+    Grid.origin = { x: 0, y: 0 };
+    return Grid;
+}());
+var grid1 = new Grid(1.0);
+var grid2 = new Grid(5.0);
+console.info(grid1.calculateDistanceFromOrigin({
+    x: 10, y: 10
+}));
+console.info(grid2.calculateDistanceFromOrigin({
+    x: 10, y: 10
+}));
+// 抽象类：抽象类作为其他派生类的基类使用。 他们一般不会被直接实例化。不同于接口，抽象类可以包含成员的实现细节。abstract关键字是用于定义抽象类和在抽象类内部定义抽象方法
+// 抽象类中的抽象方法不包含具体实现并且必须在派生类中实现。抽象方法的语法与接口方法相似。两者都是定义方法签名但不包含方法体。然而，抽象方法必须包含abstract关键字并且可以包含访问修饰符
+var Animal5 = /** @class */ (function () {
+    function Animal5() {
+    }
+    Animal5.prototype.move = function () {
+        console.info('roaming the earth');
+    };
+    return Animal5;
+}());
+var Department = /** @class */ (function () {
+    function Department(name) {
+        this.name = name;
+    }
+    Department.prototype.printName = function () {
+        console.info('Department name:', this.name);
+    };
+    return Department;
+}());
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment() {
+        return _super.call(this, 'Accounting and Auditing') || this;
+    }
+    AccountingDepartment.prototype.printMeeting = function () {
+        console.info('child');
+    };
+    AccountingDepartment.prototype.generateReports = function () {
+        console.info('generating reports...');
+    };
+    return AccountingDepartment;
+}(Department));
+var department; //允许创建一个对抽象类的引用
+// department = new Department() //不允许创建抽象类的实例
+department = new AccountingDepartment(); //允许对一个抽象子类进行实例化和赋值
+department.printName();
+department.printMeeting();
+// department.generateReports() // 方法在声明的抽象类中不存在
+//把类当做接口使用:类定义会创建两个东西：类的实例和一个构造函数。因为类可以创建出类型，所以你能够在允许使用接口的地方使用类
+var PointX = /** @class */ (function () {
+    function PointX() {
+    }
+    return PointX;
+}());
+var point3d = {
+    x1: 1,
+    y1: 2,
+    z: 3
+};
+/*
+函数
+
+
+*/
+// 完整的函数类型:注意区别定义函数类型和定义函数返回值类型的写法不同;对于函数类型定义中的返回值，我们在函数和返回值类型之前使用(=>)符号，使之清晰明了，它是函数类型的必要部分，如果函数没有返回值，也必须指定返回值类型为void而不能留空。函数的类型只是由参数类型和返回值组成的。函数中使用的捕获变量不会体现在类型里。实际上，这些变量是函数的隐藏状态并不是组成API的一部分
+var myAddFunc = function (x, y) {
+    return x + y;
+};
+// 可选参数和默认参数：使用参数名旁使用？实现可选参数的功能;可选参数必须跟在必选参数后面，如果我们希望first name是可选的，那么就必须调整它们的位置，把firstName后面
+function buildName(firstName, lastName) {
+    if (lastName) {
+        return firstName + " " + lastName;
+    }
+    else {
+        return firstName;
+    }
+}
+var r1 = buildName('Bob', 'Allen');
+var r2 = buildName('Bob');
+// let r3 = buildName('Bob','Allen', 's')
+// 剩余参数
--- src/1.ts
+++ src/1.ts
@@ -1,3 +1,5 @@
+import { deprecate } from "util";
+
 interface Options {
   name: string,
   age: number
@@ -355,3 +357,233 @@ class Animal3 {
 }
 
 // new Animal3('Cat').age //无法访问
+
+// 构造函数也可以被标记为protected。这意味着这个类不能在包含它的类外被实例化，但是能被继承
+
+class Person {
+  protected name: string;
+  protected constructor(theName: string) {
+    this.name=  theName
+  }
+}
+
+// Employee 能够继承Person
+
+class Employee extends Person {
+  private department: string;
+  constructor(name: string, department: string) {
+    super(name);
+    this.department = department
+  }
+
+  public getElevatorPitch() {
+    return `
+      Hello, my name is ${this.name} and i work in ${this.department}.
+    `
+  }
+  
+}
+
+let howard = new Employee('Howard', 'Sales')
+// let john = new Person('JOHN')   //Constructor of class 'Person' is protected and only accessible within the class declaration.
+
+
+// readonly只读属性：必须在声明时或构造函数里被初始化
+
+class Octopus {
+  readonly name: string;
+  readonly numberOfLegs: number = 8;
+  constructor(theName: string) {
+    this.name = theName
+  }
+}
+
+
+let dad = new Octopus('Man with the 8 strong legs')
+
+// dad.name = 's'  只读报错
+
+// 参数的属性：参数属性通过给构造函数参数添加一个访问限定符来声明。使用private限定一个参数属性回声明并初始化一个私有成员；对于  public和 protected来说也是一样
+
+class Animal4 {
+  // 声明和赋值结合
+  constructor(private name: string) {
+    
+  }
+
+  move(distance: number) {
+    console.info(`${this.name} moved ${distance}`);    
+  }
+}
+
+const a4 = new Animal4('doggy')
+a4.move(100)
+
+// 存取器：getter setter
+
+// 没有getter， setter的版本
+
+class Employee2 {
+  fullName: string;
+}
+
+let employee2 = new Employee2()
+employee2.fullName = 'Bob smith'
+if (employee2.fullName) {
+  console.info(employee2.fullName)
+} 
+
+// 设置了 getter， setter
+
+let password = 'secret password'
+class Employee3 {
+  private _fullName: string;
+
+  get fullName():string {
+    return this._fullName
+  }
+
+  set fullName(newName: string) {
+    if (password && password === 'secret password') {
+      this._fullName = newName
+    } else {
+      console.error('not authorized');
+    }
+  }
+}
+
+let employee3 = new Employee3()
+employee3.fullName = "Ted"
+if (employee3.fullName) {
+  console.info(employee3.fullName);
+}
+
+// 对于存取器，我们有下面几点需要注意：首先，存取器需要我们把编译器设置为输出 ecmascript 5或者更高。其次，只带有 get不带有 set的存储器自动被推断为 readonly。这在从代码生成.d.ts文件是有帮助的，因为利用这个属性的用户会看到不允许够改变它的值
+
+// 静态属性：类分为实例成员(仅当类被实例化的时候才会被初始化的属性)，静态属性(存在于类本身上面而不是类的实例上)
+
+class Grid {
+  static origin = { x: 0, y: 0 };
+  constructor(public scale: number) {
+    
+  }
+
+  // 注意此处参数的声明方式
+  calculateDistanceFromOrigin(point: { x: number; y: number; }) {
+    let xDist = (point.x - Grid.origin.x);
+    let yDist = (point.y - Grid.origin.y);
+    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale
+  }
+}
+
+let grid1 = new Grid(1.0);
+let grid2 = new Grid(5.0)
+console.info(grid1.calculateDistanceFromOrigin({
+  x: 10, y: 10
+}))
+console.info(grid2.calculateDistanceFromOrigin({
+  x: 10, y: 10
+}))
+
+
+// 抽象类：抽象类作为其他派生类的基类使用。 他们一般不会被直接实例化。不同于接口，抽象类可以包含成员的实现细节。abstract关键字是用于定义抽象类和在抽象类内部定义抽象方法
+
+// 抽象类中的抽象方法不包含具体实现并且必须在派生类中实现。抽象方法的语法与接口方法相似。两者都是定义方法签名但不包含方法体。然而，抽象方法必须包含abstract关键字并且可以包含访问修饰符
+abstract class Animal5 {
+  abstract makeSound(): void;
+  move(): void{
+    console.info('roaming the earth');
+  }
+}
+
+
+abstract class Department {
+  constructor(public name: string) {
+    
+  }
+
+  printName(): void{
+    console.info('Department name:', this.name);
+  }
+
+  abstract printMeeting(): void;
+}
+
+class AccountingDepartment extends Department {
+  constructor() {
+    super('Accounting and Auditing')
+  }
+
+  printMeeting(): void{
+    console.info('child');
+  }
+
+  generateReports(): void{
+    console.info('generating reports...');
+  }
+}
+
+let department: Department  //允许创建一个对抽象类的引用
+// department = new Department() //不允许创建抽象类的实例
+
+department = new AccountingDepartment()//允许对一个抽象子类进行实例化和赋值
+
+department.printName()
+department.printMeeting()
+// department.generateReports() // 方法在声明的抽象类中不存在
+
+//把类当做接口使用:类定义会创建两个东西：类的实例和一个构造函数。因为类可以创建出类型，所以你能够在允许使用接口的地方使用类
+
+class PointX {
+  x1: number;
+  y1: number;
+}
+
+interface Point3d extends PointX {
+  z: number;
+}
+
+
+let point3d: Point3d = {
+  x1: 1,
+  y1: 2,
+  z: 3
+}
+
+
+
+
+/* 
+函数
+
+
+*/
+
+// 完整的函数类型:注意区别定义函数类型和定义函数返回值类型的写法不同;对于函数类型定义中的返回值，我们在函数和返回值类型之前使用(=>)符号，使之清晰明了，它是函数类型的必要部分，如果函数没有返回值，也必须指定返回值类型为void而不能留空。函数的类型只是由参数类型和返回值组成的。函数中使用的捕获变量不会体现在类型里。实际上，这些变量是函数的隐藏状态并不是组成API的一部分
+
+let myAddFunc: (base: number, increment: number) => number
+  = function (x: number, y: number): number {
+    return x + y;
+  }
+
+// 可选参数和默认参数：使用参数名旁使用？实现可选参数的功能;可选参数必须跟在必选参数后面，如果我们希望first name是可选的，那么就必须调整它们的位置，把firstName后面
+
+function buildName(firstName: string, lastName?: string)
+{
+  if (lastName) {
+    return firstName + " " + lastName;
+  } else {
+    return firstName
+  }
+}
+
+let r1 = buildName('Bob','Allen')
+let r2 = buildName('Bob')
+// let r3 = buildName('Bob','Allen', 's')
+
+
+// 剩余参数
+
+
+
+  
